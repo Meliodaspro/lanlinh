c20a6049732d7478f70e433c1407c07979db25f4
Sơn <41245943+Meliodaspro@users.noreply.github.com>
2023-03-18 13:22:35 +0700

    nothing

--- js/script.js
+++ js/script.js
@@ -470,12 +470,12 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2017/12/25', function(event) {
+        $('#clock').countdown('2023/04/11', function(event) {
             var $this = $(this).html(event.strftime(''
-            + '<div class="box"><div>%D</div> <span>Days</span> </div>'
-            + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
-            + '<div class="box"><div>%M</div> <span>Mins</span> </div>'
-            + '<div class="box"><div>%S</div> <span>Secs</span> </div>'));
+            + '<div class="box"><div>%D</div> <span>Ngày</span> </div>'
+            + '<div class="box"><div>%H</div> <span>Giờ</span> </div>'
+            + '<div class="box"><div>%M</div> <span>Phút</span> </div>'
+            + '<div class="box"><div>%S</div> <span>Giây</span> </div>'));
         });
     }
 
